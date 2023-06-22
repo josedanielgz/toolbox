fbafd62483947a3812baaac6466ddf186ae719ad
José Daniel <josedanielgz@ufps.edu.co>
2023-06-21 19:33:58 -0500

    Controlador nuevo artículo

--- src/main/java/com/toolbox/controlador/AdministradorControlador.java
+++ src/main/java/com/toolbox/controlador/AdministradorControlador.java
@@ -6,18 +6,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
 import com.toolbox.modelo.Administrador;
 import com.toolbox.modelo.Articulo;
-
 import com.toolbox.servicio.AdministradorServicio;
 import com.toolbox.servicio.ArticuloServicio;
-
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 
@@ -121,6 +119,23 @@ public class AdministradorControlador {
 		Administrador adm = this.administradorService.get(admin_id);
 		model.addAttribute("admin",adm);
 		model.addAttribute("articulosFiltrados", list);
+		return "categorias";
+	}
+
+	// Redirige al formulario donde se van a crear los artículos
+	@GetMapping("/categorias/{categoria}/crear")
+	public String formularioCrearArticulo(@PathVariable("categoria") String categoria, HttpServletRequest request, @ModelAttribute Articulo nuevoArticulo, Model model){
+		int admin_id = (int) request.getSession().getAttribute("admin_id");
+		Administrador adm = this.administradorService.get(admin_id);
+		model.addAttribute("categoriaActual", categoria);
+		model.addAttribute("admin",adm);
+		return "crear_articulo";
+	}
+	
+	// Acción de crear un artículo
+	@PostMapping("/categorias/{categoria}/crear")
+	public String accionCrearArticulo(@PathVariable("categoria") String categoria, HttpServletRequest request, @ModelAttribute Articulo nuevoArticulo, Model model){
+		int admin_id = (int) request.getSession().getAttribute("admin_id");
 		return "categorias";
 	}
 	
